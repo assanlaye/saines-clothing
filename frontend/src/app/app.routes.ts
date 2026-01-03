48cf2db9a14eded5f407c330de4e77e09a28bcc5
Alassan Saine <assanlaye10@gmail.com>
2026-01-03 19:00:09 +0000

    Added Suit Detail component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,5 +10,9 @@ export const routes: Routes = [
     path: 'suits',
     loadComponent: () => import('./features/shop/suit-list/suit-list.component').then(m => m.SuitListComponent)
   },
+  {
+    path: 'suits/:id',
+    loadComponent: () => import('./features/shop/suit-detail/suit-detail.component').then(m => m.SuitDetailComponent)
+  },
   // TODO: Add Cart and Orders routes
 ];
